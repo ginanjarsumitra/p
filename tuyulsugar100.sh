2a52e2d87380ef35f93ce5cc296c8bfe183fe265
amandasantoso <80960853+amandasantoso@users.noreply.github.com>
2021-03-19 19:40:07 +0700

    Update tuyulsugar100.sh

--- tuyulsugar100.sh
+++ tuyulsugar100.sh
@@ -1,1 +1,1 @@
-./tuyulsugar -a yespowerSUGAR -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qmwc2agauwngwj9wjf8pt2865hm2wltay7gruep.TEST -t0
+./tuyulsugar -a yespowerSUGAR -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1q63mx0v3v638a2s0jga2cykjfwnh3r75200qxa6.biandewi -t0
